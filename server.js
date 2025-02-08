0452fe885bafe706c9ca2be86c63ed3c3508a692
Tofik-Raza <120191218+Tofik-Raza@users.noreply.github.com>
2025-02-08 16:32:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.post("/decrypt", async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
 });
